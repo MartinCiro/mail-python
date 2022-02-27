661b7f6d87668fe1f6d5b6ba0c72535c2686c24e
MC <52991306+MartinCiro@users.noreply.github.com>
2022-02-27 15:26:35 -0500

    Add files via upload

--- Install.sh
+++ Install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 OS=$(uname -o)
 case $( uname -o ) in
-GNU/Linux*)   echo "linux" sudo apt install python3 python3-pip git -y && sudo pip3 install -r requirements.txt && chmod 777 enviar.py && python3 enviar.py ;;
-Android*)  echo "Android" pkg install python python3 git -y && pip3 install -r requirements.txt && chmod 777 enviar.py && python3 enviar.py ;;
+GNU/Linux*)   echo "linux" sudo apt install python3 python3 pip git -y; sudo pip3 install -r requirements.txt && chmod 777 enviar.py && echo "¿Quiere hacer una prueba?" read enter python3 enviar.py ;;
+Android*)  echo "Android" pkg install python python3 git -y; pip3 install -r requirements.txt; chmod 777 enviar.py && echo "¿Quiere hacer una prueba?" read enter python3 enviar.py ;;
 esac
 
