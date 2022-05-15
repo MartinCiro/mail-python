585595b383ee4a0365e4f09a6ba749b131486615
MC <52991306+MartinCiro@users.noreply.github.com>
2022-05-14 20:21:35 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,6 +2,6 @@
 OS=$(uname -o)
 case $( uname -o ) in
 GNU/Linux*)   echo "linux"; exec -c sudo apt install python3 python3-pip -y; chmod 777 enviar.py && echo "Instalado! No olvide modificar sus varibles De: y Para:" read enter ;;
-Android*)  echo "Android"; pkg install libtool binutils clang libxml2 libxslt python python3 git -y; pip install cython lxml yagmail;  chmod 777 enviar.py && echo "¿Quiere hacer una prueba?" read enter && python3 enviar.py ;;
+Android*)  echo "Android"; pkg install libtool binutils clang libxml2 libxslt python python3 git -y; pip install cython lxml yagmail;  chmod 777 enviar.py ;;
 esac
 
